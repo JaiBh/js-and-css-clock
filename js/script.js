83e6e70e263a874022d80c37efbe423d5df3e6dc
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2021-08-01 23:50:58 +0100

    Fixed hour hand final

--- js/script.js
+++ js/script.js
@@ -6,26 +6,29 @@ const hourHand = document.querySelector(".hour-hand")
 function setDate() {
 	const now = new Date();
 // Moving the seconds hand
-	const seconds = now.getSeconds();
-	const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 	if (seconds === 0) {
 		secondHand.style.transition = `all 0s`;
 	} // Remove the snap back effect when the second hit 0
 	secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 // Moving the minutes hand
-	const minutes = now.getMinutes();
-	const minutesDegrees = ((minutes / 60) * 360) + 90
+    const minutes = now.getMinutes();
+    const minsDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+    minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
 	if (minutes === 0) {
 		minuteHand.style.transition = `all 0s`;
 	}
-	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+	minuteHand.style.transform = `rotate(${minsDegrees}deg)`
 // Moving the hours hand
-	const hours = now.getHours();
-	const hoursDegrees = ((hours / 12) * 360) + 90
-	if (hours === 0) {
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((minutes/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+	if (hour === 0) {
 		hourHand.style.transition = `all 0s`;
 	}
-	hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`
 	console.log(seconds)
 	}
 
